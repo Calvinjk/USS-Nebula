a8e1d478ca20f7796fee94af7f5662aad14516e6
juliandailey <julianaustindailey@gmail.com>
2022-03-27 21:30:36 -0400

    final changes but for real this time
    - added static variables instead of hard coding
    - added separate x y size
    - added bottom check on shape factor

--- nebula-unity/Assets/scripts/GameManager.cs
+++ nebula-unity/Assets/scripts/GameManager.cs
@@ -12,6 +12,8 @@ public class GameManager : MonoBehaviour {
 	public bool _______________;
 
 	public Map map;
+    public static int MIN_ALLOWED_ROOM_DIAM = 3;
+    public static int MIN_ALLOWED_SIZE = 50;
 
 	void Awake(){
 		DontDestroyOnLoad(this.gameObject);
@@ -33,22 +35,20 @@ public class GameManager : MonoBehaviour {
     =
     =
     */
-    int[] SetRoomDiameterRange(){
+    int[] GetRoomDiameterRange(){
 		int minRoomDiameter = 1;
-        if (GameObject.Find("MinRoomDiameter").GetComponent<Text>().text == "" || minRoomDiameter == 3)
+        if (GameObject.Find("MinRoomDiameter").GetComponent<Text>().text == "")
             minRoomDiameter = -1;
         else
             minRoomDiameter = int.Parse(GameObject.Find("MinRoomDiameter").GetComponent<Text>().text);
-        if (minRoomDiameter < 3)
-            minRoomDiameter = 3;
+        if (minRoomDiameter < MIN_ALLOWED_ROOM_DIAM)
+            minRoomDiameter = MIN_ALLOWED_ROOM_DIAM;
 
 		int maxRoomDiameter;
         if (GameObject.Find("MaxRoomDiameter").GetComponent<Text>().text == "")
             maxRoomDiameter = -1;
         else
             maxRoomDiameter = int.Parse(GameObject.Find("MaxRoomDiameter").GetComponent<Text>().text);
-        if (maxRoomDiameter > 20)
-            maxRoomDiameter = 20;
         if (minRoomDiameter > maxRoomDiameter){
             int temp = minRoomDiameter;
             minRoomDiameter = maxRoomDiameter;
@@ -58,41 +58,52 @@ public class GameManager : MonoBehaviour {
 		int[] range = {minRoomDiameter, maxRoomDiameter};
 		return range;
     }
-    float SetShapeFactor(){
+    float GetShapeFactor(){
 		float shapeFactor;
         if (GameObject.Find("ShapeFactor").GetComponent<Text>().text == "")
-            shapeFactor = -1f;
+            shapeFactor = -11f;
         else
             shapeFactor = float.Parse(GameObject.Find("ShapeFactor").GetComponent<Text>().text);
         if (shapeFactor > 10f)
             shapeFactor = 10f;
+        if (shapeFactor < -10f)
+            shapeFactor = -10f;
 		return shapeFactor;
     }
-    int SetMaxAttempts(){
+    int GetMaxAttempts(){
 		int maxAttempts;
         if (GameObject.Find("MaxAttempts").GetComponent<Text>().text == "")
             maxAttempts = -1;
         else
             maxAttempts = int.Parse(GameObject.Find("MaxAttempts").GetComponent<Text>().text);
-		return maxAttempts;
+		if (maxAttempts < 1)
+            maxAttempts = 1;
+        return maxAttempts;
     }
-    int SetSize(){
+
+    int GetXSize(){
         int tempsize;
-        if (GameObject.Find("Size").GetComponent<Text>().text != "")
-            tempsize = int.Parse(GameObject.Find("Size").GetComponent<Text>().text);
-        else
+        if (GameObject.Find("XSize").GetComponent<Text>().text == "")
             tempsize = -1;
-        if (tempsize < 50)
-            tempsize = 50;
+        else
+            tempsize = int.Parse(GameObject.Find("Size").GetComponent<Text>().text);
+        if (tempsize < MIN_ALLOWED_SIZE)
+            tempsize = MIN_ALLOWED_SIZE;
+        return tempsize;
+    }
+
+    int GetYSize(){
+        int tempsize;
+        if (GameObject.Find("YSize").GetComponent<Text>().text == "")
+            tempsize = -1;
+        else
+            tempsize = int.Parse(GameObject.Find("Size").GetComponent<Text>().text);
+        if (tempsize < MIN_ALLOWED_SIZE)
+            tempsize = MIN_ALLOWED_SIZE;
         return tempsize;
     }
 
     public Map MakeMeAMap(){
-        int[] diameterrange = SetRoomDiameterRange();
-        float sfactor = SetShapeFactor();
-        int maxattempts = SetMaxAttempts();
-        int returnsize = SetSize();
-
-        return generatorScript.GenerateMap(xSize: SetSize(), ySize: SetSize(), maxDiam: SetRoomDiameterRange()[1], minDiam: SetRoomDiameterRange()[0], sFact: SetShapeFactor(), maxAtmp: SetMaxAttempts());
+        return generatorScript.GenerateMap(xSize: GetXSize(), ySize: GetYSize(), maxDiam: GetRoomDiameterRange()[1], minDiam: GetRoomDiameterRange()[0], sFact: GetShapeFactor(), maxAtmp: GetMaxAttempts());
     }
 }
